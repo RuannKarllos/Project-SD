a339141641774e6ed5d1de53cecba33c0ef7207f
RuannKarllos <ruannkarlloss@outlook.com>
2018-12-08 14:28:23 -0300

    project

---
+++ ProjectP2/src/br/com/sd/AdventureWorks/IAdventureWorks.java
@@ -0,0 +1,13 @@
+package br.com.sd.AdventureWorks;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface IAdventureWorks extends Remote {
+	
+	public void sendRequest(Integer id, String phone) throws RemoteException;
+	public HashMap<Integer, ArrayList> listRequest() throws RemoteException;
+	
+}
